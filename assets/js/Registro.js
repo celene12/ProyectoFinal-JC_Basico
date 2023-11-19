f66662b3005d33bc4885aafc739a0c804ef257f9
celene12 <celeneparra12@gmail.com>
2023-11-19 17:38:14 -0500

    Actualización #5 - Registro listo

---
+++ assets/js/Registro.js
@@ -0,0 +1,20 @@
+var a = document.getElementById("InicioSesion");
+var b = document.getElementById("registro1");
+var c = document.getElementById("elegir");
+
+function registro1(){
+    a.style.left = "-25rem";
+    b.style.left    = "3.125rem";
+    c.style.left   = "7.85rem";
+    c.style.width = "7.188rem";
+    
+}
+
+function InicioSesion(){
+    a.style.left = "3.125rem";
+    a.style.top = "9.5rem";
+    b.style.left    = "28.125rem";
+    c.style.left   = "0rem";
+    c.style.width = "8.75rem"; 
+    
+}
